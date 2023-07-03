dfde7c7b044769a1b5a18ff9b2faba4a642c880e
Nhat Doan <nhat.doan@sap.com>
2023-07-03 10:16:12 -0400

    fix error message for phone number format

--- srv/contact-service.js
+++ srv/contact-service.js
@@ -3,15 +3,26 @@
 const TextBundle = require('@sap/textbundle').TextBundle
 
 module.exports = cds.service.impl(srv => {
+    
     srv.on('error', async (err, req) => {
+        console.log('err', err)
         const locale = req.user.locale
         const bundle = new TextBundle('../_i18n/i18n', locale)
-        err.details.forEach(e => {
-            if (e.code === 'ASSERT_FORMAT') {
-                e.args = [...e.args, bundle.getText(e.element)]
-            }
-        })
+        if (err.details) {
+            // multiple error
+            err.details.forEach(e => {
+                if (e.code === 'ASSERT_FORMAT') {
+                    console.log('element', e.element)
+                    e.args = [...e.args, bundle.getText(e.element)]
+                }
+            })
+        }
+        else {
+            // single error
+            err.args = [...err.args, bundle.getText(err.element)]
+        }
     
     })
+    
     
 })
